d28382e614b7b73bf67a56fe7d45e40b3a16c4b0
IlMelons <melonss2002@gmail.com>
2024-11-13 17:24:07 +0100

    Minor Changes

--- server/functions.lua
+++ server/functions.lua
@@ -1,1 +1,14 @@
 if GetResourceState("ox_lib") ~= "started" then return end
+
+SHConfig = require "config.config"
+
+server = {}
+
+function server.Notify(msg, type)
+    TriggerClientEvent("ox_lib:notify", source, {
+        title = SHConfig.Notify.title,
+        description = msg,
+        position = SHConfig.Notify.position,
+        type = type,
+    })
+end
